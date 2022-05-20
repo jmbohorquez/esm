8936db8143b97aff9540a245a58684e699a55b89
Julian Bohorquez <jm.bohorquez@gmail.com>
2022-05-20 14:32:47 +0200

    cambio meta tags para imagen whatsapp

--- components/Layout.js
+++ components/Layout.js
@@ -10,6 +10,11 @@ const Layout = ({ children }) => {
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon16x16.png" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/favicon180X180.png" />
+                <meta property="og:site_name" content="En Sus Marcas" />
+                <meta property="og:title" content="En Sus Marcas" />
+                <meta property="og:description" content="Agencia de estrategia y soluciones en marca, marketing y despliegue digital" />
+                <meta property="og:image" content="/ojo-wap.png" />
+                <meta property="og:type" content="website" />       
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="use-credentials" />
                 <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@300;400&family=Merriweather:wght@400;700&family=Readex+Pro:wght@300;400;500&display=swap" rel="stylesheet" />
